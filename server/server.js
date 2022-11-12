f8fffbec15028dacdee816312dfd613eeac013a1
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-11-11 21:21:25 -0800

    Fixed dependencies

--- server/server.js
+++ server/server.js
@@ -30,27 +30,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-// db.once("open", () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-//   console.log(
-//     `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-//   );
-// });
-
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-
-  db.once("open", () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
-    });
-  });
-};
-
-// Call the async function to start the server
-startApolloServer(typeDefs, resolvers);
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
