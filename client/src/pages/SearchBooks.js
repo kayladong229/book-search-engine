a476bac4aacf44777d253fc8200d97c3709aa52a
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-11-12 20:27:36 -0800

    Fixed mutations path in SearchBooks.js and added query to SavedBooks.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -13,7 +13,7 @@ import { useMutation } from "@apollo/client";
 import Auth from "../utils/auth";
 import { searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
-import { SAVE_BOOK } from "../../utils/mutations";
+import { SAVE_BOOK } from "..//utils/mutations";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
