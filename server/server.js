17efac34e31cb725fadc16fcd9322f9fdcf28d16
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-11-12 23:10:08 -0800

    Fixed login mutation

--- server/server.js
+++ server/server.js
@@ -31,5 +31,8 @@ app.get("*", (req, res) => {
 });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
