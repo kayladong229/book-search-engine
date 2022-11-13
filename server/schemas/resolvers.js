70711fa162caa4c62744cd01f32a4f4bbd601a21
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-11-12 22:43:10 -0800

    Modified resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -54,7 +54,7 @@ const resolvers = {
     },
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
+        const updatedUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
           { $pull: { savedBooks: { bookId } } },
           { new: true }
